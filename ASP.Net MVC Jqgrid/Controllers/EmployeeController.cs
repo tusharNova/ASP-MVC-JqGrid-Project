a7f985dfb57cfefd18e076fa80506970466a9aaa
Tushar Mankar <tusharmankar02@gmail.com>
2024-08-17 20:58:47 +0530

    Add select query

--- ASP.Net MVC Jqgrid/Controllers/EmployeeController.cs
+++ ASP.Net MVC Jqgrid/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using ASP.Net_MVC_Jqgrid.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 
 namespace ASP.Net_MVC_Jqgrid.Controllers
@@ -11,7 +12,7 @@ namespace ASP.Net_MVC_Jqgrid.Controllers
 		string ConnectionString = "Server=DESKTOP-UAD5JOD;Database=dbTest2;Trusted_Connection=True;MultipleActiveResultSets=True;Encrypt=False";
 		public ActionResult Index()
 		{
-			return View(new EmployeeModel());
+			return View();
 		}
 		public ActionResult Create()
 		{ 
@@ -34,25 +35,43 @@ namespace ASP.Net_MVC_Jqgrid.Controllers
 			}
 			return RedirectToAction("Index");
 		}
-		//public 
+
+		
 		public JsonResult GetEmployees()
 		{
-			var employees = new List<EmployeeModel>();
-
-			// Generate 50 demo employees
-			for (int i = 1; i <= 50; i++)
+			
+			DataTable dt = new DataTable();
+			SqlConnection sqlconnection = new SqlConnection();
+			sqlconnection.ConnectionString = ConnectionString;
+			sqlconnection.Open();
+			SqlCommand cmd = new SqlCommand();
+			cmd = sqlconnection.CreateCommand();
+			SqlDataReader res;
+			cmd.CommandText = "select * from Employees;";
+			res = cmd.ExecuteReader();
+			dt.Load(res);
+			sqlconnection.Close();
+			//using (SqlConnection sqlConn = new SqlConnection(ConnectionString)) 
+			//{
+			//	sqlConn.Open();
+			//	string query = "Select * from Employees";
+			//	SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlConn);
+			//	sqlDa.Fill(dt);
+			//}
+			List<EmployeeModel> employeeList = new List<EmployeeModel>();
+			for (int i = 1; i <= dt.Rows.Count; i++)
 			{
-				employees.Add(new EmployeeModel
-				{
-					Id = i,
-					Name = $"Employee {i}",
-					Position = GetRandomPosition(),
-					Age = new Random().Next(20, 60),
-					Office = GetRandomOffice()
-				});
+				EmployeeModel p =  new EmployeeModel();
+				p.Id = Convert.ToInt32(dt.Rows[i]["Id"].ToString());
+				p.Name = dt.Rows[i]["Name"].ToString();
+				p.Position = dt.Rows[i]["Name"].ToString();
+				p.Age = Convert.ToInt32(dt.Rows[i]["Age"].ToString());
+				p.Office = dt.Rows[i]["Office"].ToString();
+				
+				employeeList.Add(p);
 			}
 
-			return Json(employees);
+			return Json(employeeList);
 		}
 		private string GetRandomPosition()
 		{
