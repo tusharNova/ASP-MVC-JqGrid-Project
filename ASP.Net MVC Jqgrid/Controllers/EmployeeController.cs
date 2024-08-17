f6eb57c41b02afd6866cc8e6fcef56694ee53041
Tushar Mankar <tusharmankar02@gmail.com>
2024-08-17 20:20:54 +0530

    Create Emp for method

--- ASP.Net MVC Jqgrid/Controllers/EmployeeController.cs
+++ ASP.Net MVC Jqgrid/Controllers/EmployeeController.cs
@@ -1,14 +1,40 @@
 ﻿using ASP.Net_MVC_Jqgrid.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
 
 namespace ASP.Net_MVC_Jqgrid.Controllers
 {
+	
 	public class EmployeeController : Controller
 	{
-		public IActionResult Index()
+		string ConnectionString = "Server=DESKTOP-UAD5JOD;Database=dbTest2;Trusted_Connection=True;MultipleActiveResultSets=True;Encrypt=False";
+		public ActionResult Index()
 		{
+			return View(new EmployeeModel());
+		}
+		public ActionResult Create()
+		{ 
 			return View();
 		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(EmployeeModel employeeModel)
+		{
+			using (SqlConnection sqlConn = new SqlConnection(ConnectionString))
+			{
+				sqlConn.Open();
+				string query = "INSERT INTO Employees values(@Name ,@Position ,@Age ,@Office)";
+				SqlCommand cmd = new SqlCommand(query , sqlConn);
+				cmd.Parameters.AddWithValue("@Name", employeeModel.Name);
+				cmd.Parameters.AddWithValue("@Position", employeeModel.Position);
+				cmd.Parameters.AddWithValue("@Age", employeeModel.Age);
+				cmd.Parameters.AddWithValue("@Office", employeeModel.Office);
+				cmd.ExecuteNonQuery();	
+			}
+			return RedirectToAction("Index");
+		}
+		//public 
 		public JsonResult GetEmployees()
 		{
 			var employees = new List<EmployeeModel>();
